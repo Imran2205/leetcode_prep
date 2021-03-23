beb510916cd2403f5aa7b650f75bd9e50140794e
imran <imrankabir1996@gmail.com>
2021-03-24 00:48:43 +0600

    new problem

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,11 +1,19 @@
 height = [4,3,2,1,4]
 
-area = []
-
-for i in range(len(height)):
+"""for i in range(len(height)):
     for j in range(len(height[0:i+1])):
         h = min([height[i], height[j]])
         w = abs(i-j)
         a = h*w
         area.append(a)
-print(max(area))
+print(max(area))"""
+
+area = 0
+for i in range(len(height)):
+    for j in range(len(height[0:i+1])):
+        h = min([height[i], height[j]])
+        w = abs(i-j)
+        a = h*w
+        if a>area:
+            area = a
+print(area)
