278d7d907da087a7c9e63771238f14147d1ba3a8
imran <imrankabir1996@gmail.com>
2021-04-05 00:04:35 +0600

    new problem

--- longest_com_prefix.py
+++ longest_com_prefix.py
@@ -2,5 +2,28 @@ strs = ["flower","flow","flight"]
 
 out = ""
 matched = True
+y = 0
+if len(strs)<2:
+    matched = False
+    if len(strs) == 1:
+        y = len(strs[0]) + 1
 
 
+while matched:
+    for x in range(len(strs) - 1):
+        if y < len(strs[x]) and y < len(strs[x+1]):
+            if strs[x][y] == strs[x+1][y]:
+                matched = True
+            else:
+                matched = False
+                break
+        else:
+            matched = False
+            break
+    y = y + 1
+
+for i in range(y-1):
+    out = out + strs[0][i]
+
+
+print(out)
